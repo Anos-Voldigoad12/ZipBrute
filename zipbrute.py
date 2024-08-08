5b50cf7a7648b6520057d60a8db335ef50532c3a
Anos Voaldigoad <83455762+Anos-Voldigoad12@users.noreply.github.com>
2024-08-08 19:51:38 +0530

    Add files via upload

---
+++ zipbrute.py
@@ -0,0 +1,29 @@
+import zipfile
+import sys
+
+def check_password(zip_ref, password):
+    try:
+        zip_ref.setpassword(pwd=bytes(password,'utf-8'))
+        zip_ref.testzip()
+        return True
+    except RuntimeError:
+        return False
+    except Exception:
+        return False
+
+if len(sys.argv)!=3:
+    print("Usage: zipbrute.py <zip> <wordlist>")
+try:
+    zip_ref = zipfile.ZipFile(sys.argv[1],'r')
+    with open(sys.argv[2],'r') as passwords:
+        for password in passwords.readlines():            
+            if check_password(zip_ref,password.strip()):
+                print()
+                print("\033[33mFound the Correct Password: ",password.strip(),"\033[m")
+                print()
+                break
+            else:
+                print("Skipping: ",password.strip())
+except zipfile.BadZipfile:
+    print("Invalid zip file")
+    
